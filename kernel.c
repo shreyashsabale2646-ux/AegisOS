f60e050defcb88e47ec611a155b06a18016296d8
Ashishkumawat4900 <ashishkumawat4900@gmail.com>
2026-03-31 17:23:09 +0000

    kernel: changed output to KR

--- kernel.c
+++ kernel.c
@@ -1,9 +1,9 @@
 void kernel_main() {
     char *video = (char*) 0xb8000;
 
-    video[0] = 'O';
+    video[0] = 'K';
     video[1] = 0x07;
 
-    video[2] = 'S';
+    video[2] = 'R';
     video[3] = 0x07;
 }
