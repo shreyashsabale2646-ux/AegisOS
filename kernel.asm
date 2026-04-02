160e2d1029a77d23cf1c9749837e3fb28635690d
Ashishkumawat4900 <ashishkumawat4900@gmail.com>
2026-04-02 15:17:28 +0000

    kernel: multi-line output added

--- kernel.asm
+++ kernel.asm
@@ -2,22 +2,32 @@
 
 mov ah, 0x0e
 
-mov al, 'K'
+; print first message
+mov si, msg1
+call print
+
+; newline
+mov al, 0x0D
+int 0x10
+mov al, 0x0A
 int 0x10
 
-mov al, 'E'
-int 0x10
-
-mov al, 'R'
-int 0x10
-
-mov al, 'N'
-int 0x10
-
-mov al, 'E'
-int 0x10
-
-mov al, 'L'
-int 0x10
+; print second message
+mov si, msg2
+call print
 
 jmp $
+
+print:
+    mov al, [si]
+    cmp al, 0
+    je done
+    int 0x10
+    inc si
+    jmp print
+
+done:
+    ret
+
+msg1 db 'KERNEL STAGE 2 OK', 0
+msg2 db 'READY FOR NEXT STAGE', 0
