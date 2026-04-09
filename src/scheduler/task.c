752b334854db0e0627005b6db3a85638b9e840c7
Shreyash <shreyashsabale2646@gmail.com>
2026-04-09 20:31:24 +0530

    Fixed infinite loop in multitasking and ensured controlled execution

--- src/scheduler/task.c
+++ src/scheduler/task.c
@@ -1,21 +1,27 @@
-#include "../../drivers/screen.h"
+#include "drivers/screen.h"
+
 void task1() {
-    print_string("\n[Task1 running]");
+    print_string("\n[Task1 executing]");
 }
 
 void task2() {
-    print_string("\n[Task2 running]");
+    print_string("\n[Task2 executing]");
 }
 
 void run_tasks() {
-    for (int k=0 ;k<5;k++) {
+
+    print_string("\nStarting task execution...\n");
+
+    for (int k = 0; k < 5; k++) {   // ONLY 5 TIMES (IMPORTANT)
+
         task1();
 
-        for (int i = 0; i < 10000000; i++);
+        for (int i = 0; i < 5000000; i++);   // delay
 
         task2();
 
-        for (int i = 0; i < 10000000; i++);
+        for (int i = 0; i < 5000000; i++);   // delay
     }
-print_string("\nTasks completed\n");
+
+    print_string("\nTasks completed\n");
 }
